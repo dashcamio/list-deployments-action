69d75f22bd5ea7a26003d81ff7638e9c623adb38
Ian Jennings <ian@meetjennings.com>
2024-04-11 20:44:05 -0500

    try with different ref

--- index.js
+++ index.js
@@ -14,51 +14,42 @@ const github = require('@actions/github');
         const output = [];
         const seenEnvironments = [];
 
-        core.info('looking for ' + ref)
-
         for await (const {data: deployments} of octokit.paginate.iterator(
             octokit.repos.listDeployments,
             {
                 owner,
-                repo
+                repo,
+                ref
             }
         )) {
-
             for (const deployment of deployments) {
-            //     core.info(`Deployment: ${deployment.environment} (#${deployment.id})`);
-            //     if (seenEnvironments[deployment.environment]) {
-            //         continue;
-            //     }
-            //     const {data: statuses } = await octokit.repos.listDeploymentStatuses({
-            //         owner,
-            //         repo,
-            //         deployment_id: deployment.id
-            //     });
-            //     // Ignore deployments without statuses
-            //     if (statuses.length < 1) {
-            //         continue;
-            //     }
-
-            core.info(deployment.sha + ' ' + deployment.url + ' ' + deployment.ref)
-
-            if (deployment.ref == ref) {
-                core.info('MATCHES!!!!')
-            }
-
+                core.info(`Deployment: ${deployment.environment} (#${deployment.id})`);
+                if (seenEnvironments[deployment.environment]) {
+                    continue;
+                }
+                const {data: statuses } = await octokit.repos.listDeploymentStatuses({
+                    owner,
+                    repo,
+                    deployment_id: deployment.id
+                });
+                // Ignore deployments without statuses
+                if (statuses.length < 1) {
+                    continue;
+                }
                 
-            //     core.info(`Status: ${deployment.environment} (#${deployment.id}) ${statuses[0].state}`);
-            //     // The list of statuses is sorted on last to first.
-            //     // If it is not queued, this deployment has already been handled.
-            //     seenEnvironments[deployment.environment] = true;
-            //     output.push({
-            //         environment: deployment.environment,
-            //         deployment_id: deployment.id,
-            //         deployment_url: deployment.url,
-            //         status: statuses[0].state,
-            //         ref: deployment.ref,
-            //         deployment: deployment,
-            //         deployment_status: statuses[0]
-            //     });
+                core.info(`Status: ${deployment.environment} (#${deployment.id}) ${statuses[0].state}`);
+                // The list of statuses is sorted on last to first.
+                // If it is not queued, this deployment has already been handled.
+                seenEnvironments[deployment.environment] = true;
+                output.push({
+                    environment: deployment.environment,
+                    deployment_id: deployment.id,
+                    deployment_url: deployment.url,
+                    status: statuses[0].state,
+                    ref: deployment.ref,
+                    deployment: deployment,
+                    deployment_status: statuses[0]
+                });
             }
         }
         core.info(`Matrix: ${JSON.stringify(output)}`);
